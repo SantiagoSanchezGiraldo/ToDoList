aa5eb19cddc7be8cc0a9232baea00fa6261316ff
Santiago Sánchez Giraldo <santiagosanchezgiraldo@pascualbravo.edu.co>
2024-05-26 20:32:34 -0500

    Últimos retoques
    Se le añadió unos últimos retoques al estilo de la pagina

--- src/components/Deafualt.jsx
+++ src/components/Deafualt.jsx
@@ -1,9 +1,12 @@
+import paginaNo from '../assets/paginaNo.png';
 import React from 'react'
 
 export const Deafualt = () => {
     return (
-        <div>
+        <div className='defualt container'>
             <h1>Esta pagina no se encuentra</h1>
+            <br />
+            <img src={paginaNo} alt="" />
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 export const Home = () => {
     return (
         <div className='home container'>
-            <h1>Bienvenido a ToDo</h1>
-            <br /><br />
+            <h1>Bienvenido a TaskMaster</h1>
             <p>
-                <h3>Esta pagina te ayudara a mantener el orden de tus obejtivos y metas,
-                por medio de:</h3> <br />
+                <h2>Esta pagina te ayudara a mantener el orden de tus obejtivos y metas,
+                por medio de:</h2> <br />
                 - Una Lista de tareas: Puedes agregar tus deberes, objetivos y metas a tener
-                en cuenta, podras editar la tarea que agregaste al igual que eliminarla y chulearlo
+                en cuenta, pod  ras editar la tarea que agregaste al igual que eliminarla y chulearlo
                 de tu lista de tareas pendientes. <br /><br />
                 - Un Calendario: Puedes agendar tus tareas por medio de fechas, tambien podras editar 
                 la tarea que agregaste al igual que eliminarla y chulearlo de tu lista de tareas 
                 pendientes. <br /><br />
                 - Un Gestor de Presupuesto: Puedes gestionar el presupuesto de tus tareas u objetivos,
                 podras poner un presupuesto inicial y a medida que vas agregando los gastos que relacionas 
-                con este presupuesto se va realizando tus cuentas.
+                con este presupuesto se va realizando tus cuentas. <br /><br />
+                - Un Cotizador de Prestamos: Podras cotizar tus prestamos, ingresa un valor o la cantidad
+                del prestamos y el plazo para pagar dicho prestamo para hacer su calculo. 
             </p>
         </div>
     )
